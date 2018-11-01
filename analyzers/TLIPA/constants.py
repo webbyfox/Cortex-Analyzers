ab4039653610b4a86f659ed7fa1c7fe9b97c9045
Rizwan Mansuri <Rizwan@webbyfox.com>
2018-11-01 17:08:43 +0000

    Added base file structure for TLIPA analyzer

---
+++ analyzers/TLIPA/constants.py
@@ -0,0 +1,1 @@
+TLIPA_API = 'http://tlipa-api.tlipa.nettitude.info'
---
+++ analyzers/TLIPA/tlipa.py
@@ -0,0 +1,26 @@
+from .constants import TLIPA_API
+
+from cortexutils.analyzer import Analyzer
+
+
+class TLIPA(Analyzer):
+
+    def run(self):
+        Analyzer.run(self)
+
+        if self.data_type == 'ip':
+            try:
+                data = self.get_data()
+               #Todo: Query TLIPA API to get data
+
+            except ValueError as e:
+                self.error('Invalid IP address')
+            except AddressNotFoundError as e:
+                self.error('Unknown IP address')
+            except Exception as e:
+                self.unexpectedError(type(e))
+        else:
+            self.notSupported()
+
+if __name__ == '__main__':
+    TLIPA().run()
